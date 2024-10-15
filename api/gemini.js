14a2dfcf0baee84bec50d9b09316e266f9d4a077
A-pinch-ofsalt <fhiciano5@gmail.com>
2024-10-15 18:47:25 -0400

    Used code from working server to fix this one

--- api/gemini.js
+++ api/gemini.js
@@ -8,14 +8,15 @@ export default async function handler(req, res) {
       console.log("Processing request");
 
         // Initialize the Gemini API
-        console.log(`API KEY = ${process.env.GEMINI_API_KEY}`)
-        const gemini = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-        const model = gemini.getGenerativeModel({ model: 'gemini-pro' });
+    console.log(`API KEY = ${process.env.GEMINI_API_KEY}`)
+    const gemini = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+    const model = gemini.getGenerativeModel({ model: 'gemini-pro' });
 
         // Start a conversation with the Gemini API
-    const conversation = model.startChat({
+    const conversation = await model.startChat({
         history: [
-            { role: 'user', parts: [`You will be given some questions based on the following context: ${context}`] },
+            { role: 'user', parts: [`Content: ${context}`] },
+            //{ role: 'user', parts: [`You will be given some questions based on the following context: ${context}`] },
             { role: 'user', parts: ["Respond to the questions as accurately as you can. Type your responses in the following format: ALOHA{answer 1> answer 2> etc}. Your responses will be separated by a script using this format, so don't deviate from it. Each answer will be shown to the user directly, so any formatting (such as newlines) will be extremely confusing."] },
           ],
           generationConfig: { maxOutputTokens: 300 },
